f06b23a06620b04de44681647e3000e05ee6dbf1
ibakula <ivan.bakula@outlook.com>
2022-12-30 22:19:07 +0100

    Fix some compiler flags

---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import { createRoot } from 'react-dom/client';
+import TestComponent from 'components/test-component';
+
+const container = document.getElementById("root") as Element;
+const root = createRoot(container);
+
+root.render(<TestComponent />);
