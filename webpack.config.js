3501c01a82bc1ad5f944db40d6e0450ab47dc88c
ibakula <ivan.bakula@outlook.com>
2023-01-03 18:29:34 +0100

    Configured webpack for css/sass and css modules bundling

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,25 @@
 const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  mode: 'development',
-  devtool: 'source-map',
+  mode: 'production',
+  devtool: (this.mode === "development" ? "source-map" : false),
   entry: './source/index.tsx',
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        styles: {
+          name: "bundle",
+          type: "css/mini-extract",
+          chunks: "all",
+          enforce: true,
+        }
+      }
+    }
+  },
+  plugins: [ 
+    new MiniCssExtractPlugin()
+  ],
   module: {
     rules: [
       {
@@ -11,14 +27,82 @@ module.exports = {
         use: 'ts-loader',
         exclude: /node_modules/,
       },
-    ],
+      {
+        test: /(?<!\.module)\.((c|sa|sc)ss)$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 2,
+              sourceMap: true,
+              modules: {
+                mode: "icss"
+              }
+            }
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: [
+                  [
+                    "postcss-preset-env"
+                  ],
+                ],
+              }
+            }
+          },
+          "sass-loader"
+        ]
+      },
+      {
+        test: /\.module\.((c|sa|sc)ss)$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 2,
+              sourceMap: true,
+              modules: {
+                mode: "local",
+                namedExport: false, // This disables requirement of destructuring a css import i.e. { camelCasedClassName }
+                localIdentName: "[name]__[local]_[hash:base64]"
+              }
+            }
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: [
+                  [
+                    "postcss-preset-env"
+                  ],
+                ],
+              }
+            }
+          },
+          "sass-loader"
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|eot|ttf|woff|woff2)$/i,
+        type: "asset",
+        generator: {
+          outputPath: 'static/'
+        }
+      }
+    ]
   },
   resolve: {
-    extensions: ['.tsx', '.ts', "js"],
+    extensions: ['.tsx', '.ts', ".js"]
   },
   output: {
     filename: 'bundle.js',
-    sourceMapFilename: "bundle.js.map",
-    path: path.resolve(__dirname, 'build')
-  },
+    sourceMapFilename: "[name]-[id].js.map",
+    path: path.resolve(__dirname, 'build'),
+    clean: true
+  }
 };
