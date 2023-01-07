181d78d06919c7b8a24594249e86ee1e1b9a1c71
ibakula <ivan.bakula@outlook.com>
2023-01-07 23:43:34 +0100

    fix inclusion of assets

--- jest.config.ts
+++ jest.config.ts
@@ -3,4 +3,12 @@ import { jsWithTs as defaults } from 'ts-jest/presets';
 
 const config: JestConfigWithTsJest = Object.assign({}, defaults);
 
+config.moduleFileExtensions = ['tsx', 'ts', 'js', 'mjs', 'cjs', 'jsx', 'json', 'node'];
+
+config.moduleNameMapper = Object.assign({
+  '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+    '<rootDir>/__mocks__/fileMock.ts',
+  '\\.(css|less)$': 'identity-obj-proxy',
+}, config.moduleNameMapper);
+
 export default config;
