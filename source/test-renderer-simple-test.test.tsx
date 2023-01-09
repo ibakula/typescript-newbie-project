e335d395cabb364ed033dbf1ae4c1b467697342a
ibakula <ivan.bakula@outlook.com>
2023-01-09 22:17:46 +0100

    removed fake timers since our code no longer uses async timeout funcs

--- source/test-renderer-simple-test.test.tsx
+++ source/test-renderer-simple-test.test.tsx
@@ -3,19 +3,13 @@ import TestFNComponent from './components/test-fn-component';
 import TestComponent from './components/test-component';
 
 test('Test component changes after prop update', function () {
-  jest.useFakeTimers();
   let testRenderer: any = null;
   act(function() {
     testRenderer = create(<TestComponent requiredProp="remain the same" />);
   });
-  jest.clearAllTimers();
   expect(testRenderer!.toJSON()).toMatchSnapshot();
-
   act(function() {
     testRenderer.update(<TestComponent requiredProp="differ in something" />);
   });
-  jest.clearAllTimers();
   expect(testRenderer!.toJSON()).toMatchSnapshot();
-
-  jest.useRealTimers();
 });
