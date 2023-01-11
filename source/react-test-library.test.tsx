3a0d36f5a89bd6813b59eb53b6977cc0d91ee6a3
ibakula <ivan.bakula@outlook.com>
2023-01-11 20:45:02 +0100

    another test for module mocking added

--- source/react-test-library.test.tsx
+++ source/react-test-library.test.tsx
@@ -7,8 +7,21 @@ import TestFNComponent from './components/test-fn-component';
 import DragTestComponent from "./components/drag-test";
 
 afterEach(cleanup);
+beforeEach(jest.resetModules);
+
+test("Tryout of mocking a module", async function() {
+  expect.assertions(1);
+  jest.doMock("./components/drag-test", function() {
+    return () => (<div aria-label="automock-rocks">Nothing to see here! :)</div>);
+  });
+  const TestComponent = await import("./components/drag-test");
+  render(<TestComponent.default />);
+  const divElement = await screen.findByRole("generic", {name: "automock-rocks"}) as HTMLDivElement;
+  expect((divElement.firstChild as Node).textContent).toBe("Nothing to see here! :)");
+});
 
 test("Test tools from test library with some components", async function () {
+  expect.assertions(1);
   render(<TestFNComponent />);
   const [ textField ] = await screen.findAllByRole("textbox") as HTMLInputElement[];
   expect(textField.value).toBe("No required prop supplied");
@@ -17,6 +30,7 @@ test("Test tools from test library with some components", async function () {
 });
 
 test("Try out dragging and dropping testing component", async function () {
+  expect.assertions(1);
   render(<DragTestComponent />);
   const img = await screen.findByRole("img") as HTMLImageElement;
   const [ containerDiv, secondContainer, dropContainer ] = await screen.findAllByRole("generic") as HTMLDivElement[];
